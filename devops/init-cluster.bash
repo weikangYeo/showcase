8280f87644c3a4e67fff43182cce3be003ec2fb5
weikangYeo <micky.lys@gmail.com>
2025-07-12 19:40:15 +0800

    Add pre-requsite for this project, add startup and init script

---
+++ devops/init-cluster.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "spin up DB and other localstack"
+docker-compose -f db/docker-compose.yml up -d
+
+echo "creating k3d cluster with registry and port mapping"
+k3d cluster create showcase-cluster --registry-create showcase-cluster-registry:0.0.0.0:5000 -p "8081:80@loadbalancer"
+
+echo "pull keycloak image and import to regisry"
+docker pull quay.io/keycloak/keycloak:26.2.5
+k3d image import quay.io/keycloak/keycloak:26.2.5 -c showcase-cluster
+
+echo "deploy keycloak"
+kubectl apply -f ./cluster/keycloak-deployment.yaml
+kubectl apply -f ./cluster/ingress.yaml
+
---
+++ devops/start-cluster.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "spin up DB and other localstack"
+docker-compose -f db/docker-compose.yml up -d
+
+echo "start k3d cluster"
+k3d cluster start showcase-cluster
+
