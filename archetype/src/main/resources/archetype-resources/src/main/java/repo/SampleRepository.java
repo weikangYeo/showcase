5e7d80f7f050acb596f36bfc227d4bb6f3e5461d
weikangYeo <micky.lys@gmail.com>
2025-08-06 18:16:24 +0800

    fixed archetype code

--- archetype/src/main/resources/archetype-resources/src/main/java/repo/SampleRepository.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/repo/SampleRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SampleRepository extends JpaRepository<Long, Sample> {
+public interface SampleRepository extends JpaRepository<Sample, Long> {
 }
